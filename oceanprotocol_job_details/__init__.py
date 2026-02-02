7a13ce748b239a45ab1390177f5108314d3cd103
spin3l <dev.christianlopezgarcia@gmail.com>
2026-02-02 15:41:15 +0100

    fix: naming

--- oceanprotocol_job_details/__init__.py
+++ oceanprotocol_job_details/__init__.py
@@ -1,4 +1,5 @@
 from .helpers import create_container, load_job_details
 from .ocean import JobDetails
+from .executors import run_in_executor
 
-__all__ = [JobDetails, load_job_details, create_container]  # type: ignore
+__all__ = [JobDetails, load_job_details, create_container, run_in_executor]  # type: ignore
